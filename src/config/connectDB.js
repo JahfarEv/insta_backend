93c246066be9c00c86f3b8ff1da523bbb9facbf1
JahfarEv <evjahfarali@gmail.com>
2024-02-23 08:03:09 +0530

    Db update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,18 +1,14 @@
-// import mongoose from "mongoose";
+// const dotenv=require('dotenv');
+// dotenv.config({path:'../.env'})
+// const mongoose=require('mongoose');
 
-//  const connectDB = async () => {
+
+// const connectDB=async()=>{
 //     try {
-      
-//         const connect = await mongoose.connect(process.env.MONGO_URI, { 
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-               
-//         })
-//         console.log(`monogoDB Connected: ${connect.connection.host}`)  
-//     } catch (error) {
-//         console.error(`Error ${error.message}`)
-//         process.exit(1)
-        
+//         await mongoose.connect(process.env.MONGODB_URI)
+//         console.log(`connect mongoDb`)
+//     } catch (err) {
+//         console.error(err)
 //     }
 // }
-// export default connectDB
+// module.exports=connectDB 
